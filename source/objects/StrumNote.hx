fb15c726939076c02c6f1eae29e0638678a32ca0
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-11-21 05:10:21 -0500

    Update StrumNote.hx

--- source/objects/StrumNote.hx
+++ source/objects/StrumNote.hx
@@ -1,171 +1,131 @@
-package objects;
+package;
 
-import backend.animation.PsychAnimationController;
-
-import shaders.RGBPalette;
-import shaders.RGBPalette.RGBShaderReference;
+import flixel.FlxSprite;
+import PlayState;
 
 class StrumNote extends FlxSprite
 {
-	public var rgbShader:RGBShaderReference;
-	public var resetAnim:Float = 0;
-	private var noteData:Int = 0;
-	public var direction:Float = 90;
-	public var downScroll:Bool = false;
-	public var sustainReduce:Bool = true;
-	private var player:Int;
-	
-	public var texture(default, set):String = null;
-	private function set_texture(value:String):String {
-		if(texture != value) {
-			texture = value;
-			reloadNote();
-		}
-		return value;
-	}
-
-	public var useRGBShader:Bool = true;
-	public function new(x:Float, y:Float, leData:Int, player:Int) {
-		animation = new PsychAnimationController(this);
-
-		rgbShader = new RGBShaderReference(this, Note.initializeGlobalRGBShader(leData));
-		rgbShader.enabled = false;
-		if(PlayState.SONG != null && PlayState.SONG.disableNoteRGB) useRGBShader = false;
-		
-		var arr:Array<FlxColor> = ClientPrefs.data.arrowRGB[leData];
-		if(PlayState.isPixelStage) arr = ClientPrefs.data.arrowRGBPixel[leData];
-		
-		if(leData <= arr.length)
-		{
-			@:bypassAccessor
-			{
-				rgbShader.r = arr[0];
-				rgbShader.g = arr[1];
-				rgbShader.b = arr[2];
-			}
-		}
-
-		noteData = leData;
-		this.player = player;
-		this.noteData = leData;
-		this.ID = noteData;
-		super(x, y);
-
-		var skin:String = null;
-		if(PlayState.SONG != null && PlayState.SONG.arrowSkin != null && PlayState.SONG.arrowSkin.length > 1) skin = PlayState.SONG.arrowSkin;
-		else skin = Note.defaultNoteSkin;
-
-		var customSkin:String = skin + Note.getNoteSkinPostfix();
-		if(Paths.fileExists('images/$customSkin.png', IMAGE)) skin = customSkin;
-
-		texture = skin; //Load texture and anims
-		scrollFactor.set();
-		playAnim('static');
-	}
-
-	public function reloadNote()
+	public var baseX:Float;
+	public var baseY:Float;
+	public var playerStrum:Bool;
+	public var pressingKey5:Bool;
+	public function new(x:Float, y:Float, type:String, strumID:Int, playerStrum:Bool)
 	{
-		var lastAnim:String = null;
-		if(animation.curAnim != null) lastAnim = animation.curAnim.name;
+		super(x, y);
+		baseY = y;
+		pressingKey5 = false;
 
-		if(PlayState.isPixelStage)
+		ID = strumID;
+
+		// get the frames and stuff
+		switch (type)
 		{
-			loadGraphic(Paths.image('pixelUI/' + texture));
-			width = width / 4;
-			height = height / 5;
-			loadGraphic(Paths.image('pixelUI/' + texture), true, Math.floor(width), Math.floor(height));
-
-			antialiasing = false;
-			setGraphicSize(Std.int(width * PlayState.daPixelZoom));
-
-			animation.add('green', [6]);
-			animation.add('red', [7]);
-			animation.add('blue', [5]);
-			animation.add('purple', [4]);
-			switch (Math.abs(noteData) % 4)
-			{
-				case 0:
-					animation.add('static', [0]);
-					animation.add('pressed', [4, 8], 12, false);
-					animation.add('confirm', [12, 16], 24, false);
-				case 1:
-					animation.add('static', [1]);
-					animation.add('pressed', [5, 9], 12, false);
-					animation.add('confirm', [13, 17], 24, false);
-				case 2:
-					animation.add('static', [2]);
-					animation.add('pressed', [6, 10], 12, false);
-					animation.add('confirm', [14, 18], 12, false);
-				case 3:
-					animation.add('static', [3]);
-					animation.add('pressed', [7, 11], 12, false);
-					animation.add('confirm', [15, 19], 24, false);
-			}
+			case '3D':
+				frames = Paths.getSparrowAtlas('notes/NOTE_assets_3D');
+			case 'top10awesome':
+				frames = Paths.getSparrowAtlas('notes/OMGtop10awesomehi');
+			case 'gh':
+				frames = Paths.getSparrowAtlas('notes/NOTEGH_assets');
+			case 'shape':
+				frames = Paths.getSparrowAtlas('notes/NOTE_assets_Shape');
+			case 'pibby':
+				frames = Paths.getSparrowAtlas('notes/Glitch_Note_assets');
+			default:
+				frames = Paths.getSparrowAtlas('notes/NOTE_assets');
 		}
-		else
+		// actually load in the animation
+		switch (type)
 		{
-			frames = Paths.getSparrowAtlas(texture);
-			animation.addByPrefix('green', 'arrowUP');
-			animation.addByPrefix('blue', 'arrowDOWN');
-			animation.addByPrefix('purple', 'arrowLEFT');
-			animation.addByPrefix('red', 'arrowRIGHT');
+			case 'gh':
+				animation.addByPrefix('green', 'A Strum');
+				animation.addByPrefix('red', 'B Strum');
+				animation.addByPrefix('yellow', 'C Strum');
+				animation.addByPrefix('blue', 'D Strum');
+				animation.addByPrefix('orange', 'E Strum');
 
-			antialiasing = ClientPrefs.data.antialiasing;
-			setGraphicSize(Std.int(width * 0.7));
+				switch (Math.abs(strumID))
+				{
+					case 0:
+						animation.addByPrefix('static', 'A Strum');
+						animation.addByPrefix('pressed', 'A Press', 24, false);
+						animation.addByPrefix('confirm', 'A Confirm', 24, false);
+					case 1:
+						animation.addByPrefix('static', 'B Strum');
+						animation.addByPrefix('pressed', 'B Press', 24, false);
+						animation.addByPrefix('confirm', 'B Confirm', 24, false);
+					case 2:
+						animation.addByPrefix('static', 'C Strum');
+						animation.addByPrefix('pressed', 'C Press', 24, false);
+						animation.addByPrefix('confirm', 'C Confirm', 24, false);
+					case 3:
+						animation.addByPrefix('static', 'D Strum');
+						animation.addByPrefix('pressed', 'D Press', 24, false);
+						animation.addByPrefix('confirm', 'D Confirm', 24, false);
+					case 4:
+						animation.addByPrefix('static', 'E Strum');
+						animation.addByPrefix('pressed', 'E Press', 24, false);
+						animation.addByPrefix('confirm', 'E Confirm', 24, false);
+				}
+			default:
+				animation.addByPrefix('green', 'arrowUP');
+				animation.addByPrefix('blue', 'arrowDOWN');
+				animation.addByPrefix('purple', 'arrowLEFT');
+				animation.addByPrefix('red', 'arrowRIGHT');
 
-			switch (Math.abs(noteData) % 4)
-			{
-				case 0:
-					animation.addByPrefix('static', 'arrowLEFT');
-					animation.addByPrefix('pressed', 'left press', 24, false);
-					animation.addByPrefix('confirm', 'left confirm', 24, false);
-				case 1:
-					animation.addByPrefix('static', 'arrowDOWN');
-					animation.addByPrefix('pressed', 'down press', 24, false);
-					animation.addByPrefix('confirm', 'down confirm', 24, false);
-				case 2:
-					animation.addByPrefix('static', 'arrowUP');
-					animation.addByPrefix('pressed', 'up press', 24, false);
-					animation.addByPrefix('confirm', 'up confirm', 24, false);
-				case 3:
-					animation.addByPrefix('static', 'arrowRIGHT');
-					animation.addByPrefix('pressed', 'right press', 24, false);
-					animation.addByPrefix('confirm', 'right confirm', 24, false);
-			}
+				var nSuf:Array<String> = ['LEFT0', 'DOWN0', 'UP0', 'RIGHT0'];
+				var pPre:Array<String> = ['left', 'down', 'up', 'right'];
+				switch (PlayState.SONG.mania)
+				{
+					case 1:
+						nSuf = ['LEFT0', 'DOWN0', 'SPACE', 'UP0', 'RIGHT0'];
+						pPre = ['left', 'down', 'white', 'up', 'right'];
+					case 2:
+						nSuf = ['LEFT0', 'UP0', 'RIGHT0', 'LEFT0', 'DOWN0', 'RIGHT0'];
+						pPre = ['left', 'up', 'right', 'yel', 'down', 'dark'];
+					case 3:
+						nSuf = ['LEFT0', 'UP0', 'RIGHT0', 'SPACE', 'LEFT0', 'DOWN0', 'RIGHT0'];
+						pPre = ['left', 'up', 'right', 'white', 'yel', 'down', 'dark'];
+					case 4:
+						nSuf = ['LEFT0', 'DOWN0', 'UP0', 'RIGHT0', 'SPACE', 'LEFT0', 'DOWN0', 'UP0', 'RIGHT0'];
+						pPre = ['left', 'down', 'up', 'right', 'white', 'yel', 'violet', 'black', 'dark'];
+					case 5:
+						nSuf = ['LEFT0', 'DOWN0', 'UP0', 'RIGHT0', 'LEFTSHARP', 'DOWNSHARP', 'UPSHARP', 'RIGHTSHARP', 'LEFT0', 'DOWN0', 'UP0', 'RIGHT0'];
+						pPre = ['left', 'down', 'up', 'right', 'pink', 'turq', 'emerald', 'lightred', 'yel', 'violet', 'black', 'dark'];
+				}
+				animation.addByPrefix('static', 'arrow' + nSuf[strumID]);
+				animation.addByPrefix('pressed', pPre[strumID] + ' press', 24, false);
+				animation.addByPrefix('confirm', pPre[strumID] + ' confirm', 24, false);
 		}
+		playAnim('static');
+
+		antialiasing = type != '3D';
+
+		setGraphicSize(Std.int(width * (type == 'gh' ? 0.7 : Note.noteSize)));
 		updateHitbox();
 
-		if(lastAnim != null)
-		{
-			playAnim(lastAnim, true);
-		}
-	}
+		scrollFactor.set();
 
-	public function playerPosition()
+		this.playerStrum = playerStrum;
+	}
+	public function resetX()
 	{
-		x += Note.swagWidth * noteData;
-		x += 50;
-		x += ((FlxG.width / 2) * player);
+		x = baseX;
 	}
-
-	override function update(elapsed:Float) {
-		if(resetAnim > 0) {
-			resetAnim -= elapsed;
-			if(resetAnim <= 0) {
-				playAnim('static');
-				resetAnim = 0;
-			}
-		}
-		super.update(elapsed);
+	public function resetY()
+	{
+		y = baseY;
 	}
+	public function centerStrum()
+	{
+		x = baseX + 320 * (playerStrum ? -1 : 1) + 78 / 4;
+	}
+    public function playAnim(anim:String, ?force:Bool = false) {
+        animation.play(anim, force);
+        updateHitbox();
+        offset.x = frameWidth / 2;
+        offset.y = frameHeight / 2;
 
-	public function playAnim(anim:String, ?force:Bool = false) {
-		animation.play(anim, force);
-		if(animation.curAnim != null)
-		{
-			centerOffsets();
-			centerOrigin();
-		}
-		if(useRGBShader) rgbShader.enabled = (animation.curAnim != null && animation.curAnim.name != 'static');
+        offset.x -= 156 * Note.scales[PlayState.SONG.mania] / 2;
+        offset.y -= 156 * Note.scales[PlayState.SONG.mania] / 2;
 	}
 }
